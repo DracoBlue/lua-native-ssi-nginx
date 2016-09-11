539b0bc9ec5dba42eed8fee8ec3858cfd0187d9c
DracoBlue <JanS@DracoBlue.de>
2016-09-11 13:19:18 +0200

    Added test for custom header

---
+++ tests/echo_custom_header.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -v -sS -X POST -H "Custom-Header: LALA" -H "Content-Type: application/json" -d '{"key":"value"}' "localhost:4778/echo/" 2>&1 | grep '< X-Custom-Header'
+exit $?
