7db201100e54b170d89f21bf73b720a67a242715
DracoBlue <JanS@DracoBlue.de>
2016-09-11 13:28:25 +0200

    Hopefully fixed the line ending issue in the tests

--- tests/echo_custom_header.sh
+++ tests/echo_custom_header.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -v -sS -X POST -H "Custom-Header: LALA" -H "Content-Type: application/json" -d '{"key":"value"}' "localhost:4778/echo/" 2>&1 | grep '< X-Custom-Header'
+curl -v -sS -X POST -H "Custom-Header: LALA" -H "Content-Type: application/json" -d '{"key":"value"}' "localhost:4778/echo/" 2>&1 | grep '< X-Custom-Header' | tr -d "\n"
 exit $?
--- tests/echo_method.sh
+++ tests/echo_method.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -v -sS -X POST -H "Content-Type: application/json" -d '{"key":"value"}' "localhost:4778/echo/" 2>&1 | grep '< X-Request-Method'
+curl -v -sS -X POST -H "Content-Type: application/json" -d '{"key":"value"}' "localhost:4778/echo/" 2>&1 | grep '< X-Request-Method' | tr -d "\n"
 exit $?
