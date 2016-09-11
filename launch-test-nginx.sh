b3be468110d5cc81fc063b64bf8bda1d3e670196
DracoBlue <JanS@DracoBlue.de>
2016-09-11 13:38:46 +0200

    daemon parameter for launch-test-nginx

--- launch-test-nginx.sh
+++ launch-test-nginx.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-cp .travis/Dockerfile . && docker build -t lua-native-ss-nginx . && docker run --rm -p127.0.0.1:4777:4777 -p127.0.0.1:4778:4778 -p127.0.0.1:4780:80 -it lua-native-ss-nginx
+DOCKER_ARGS="--rm"
+if [ "$1" == "daemon" ]
+then
+	DOCKER_ARGS="-d"
+fi
+
+cp .travis/Dockerfile . && docker build -t lua-native-ss-nginx . && docker run $DOCKER_ARGS -p127.0.0.1:4777:4777 -p127.0.0.1:4778:4778 -p127.0.0.1:4780:80 -it lua-native-ss-nginx
 exit $?
