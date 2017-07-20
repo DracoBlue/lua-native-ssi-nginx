abcedabb4b6dea429e7f69f14dc8c319482db611
DracoBlue <JanS@DracoBlue.de>
2017-07-20 13:10:25 +0100

    Update lua-ssi-content.lua

--- src/lua-ssi-content.lua
+++ src/lua-ssi-content.lua
@@ -65,11 +65,11 @@ getCacheControlFieldsFromHeaders = function(headers)
         return {}
     end
 
-    local cacheControlHeaderPrefixedAndSuffixedWithAWhitespace = " " .. cacheControlHeader .. " "
+    local cacheControlHeaderPrefixedAndSuffixedWithAWhitespace = ", " .. cacheControlHeader .. " ,"
 
     local fields = {}
     
-    for key in string.gmatch(cacheControlHeaderPrefixedAndSuffixedWithAWhitespace, '[%s,]+([^=%s,]+)[%s,]-')
+    for key in string.gmatch(cacheControlHeaderPrefixedAndSuffixedWithAWhitespace, ',[%s]+([^=%s,]+)[%s]-,')
     do
         fields[key] = true
     end
