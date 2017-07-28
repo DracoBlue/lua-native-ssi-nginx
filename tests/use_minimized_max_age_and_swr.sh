4385f217f0cbb6ad70c51d269738910a76e117a6
DracoBlue <JanS@DracoBlue.de>
2017-07-28 23:11:27 +0100

    Update use_minimized_max_age_and_swr.sh

--- tests/use_minimized_max_age_and_swr.sh
+++ tests/use_minimized_max_age_and_swr.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-curl -v http://localhost:4778/max-age/30-seconds/35-age/40-swr
-curl -v http://localhost:4777/max-age/30-seconds/35-age/40-swr
+curl -v http://localhost:4778/max-age/35-seconds/30-age/40-swr
+curl -v http://localhost:4777/max-age/35-seconds/30-age/40-swr
 
 
-curl -v -sS "localhost:4777/max-age/includes-30-max-age-35-age-40-swr-expires-in-120.json" 
-curl -v -sS "localhost:4778/max-age/includes-30-max-age-35-age-40-swr-expires-in-120.json" 
-curl -v -sS "localhost:4778/max-age/includes-30-max-age-35-age-40-swr-expires-in-120.json" 2>&1 | grep "Cache-Control" | tr -d "\n"
+curl -v -sS "localhost:4777/max-age/includes-35-max-age-30-age-40-swr-expires-in-120.json" 
+curl -v -sS "localhost:4778/max-age/includes-35-max-age-30-age-40-swr-expires-in-120.json" 
+curl -v -sS "localhost:4778/max-age/includes-35-max-age-30-age-40-swr-expires-in-120.json" 2>&1 | grep "Cache-Control" | tr -d "\n"
 exit $?
