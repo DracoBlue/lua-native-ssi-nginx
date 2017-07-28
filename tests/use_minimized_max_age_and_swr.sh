8c69a744d5fe40be3d6ba9c1ec4a308516284b5d
DracoBlue <JanS@DracoBlue.de>
2017-07-28 22:56:20 +0100

    Update use_minimized_max_age_and_swr.sh

--- tests/use_minimized_max_age_and_swr.sh
+++ tests/use_minimized_max_age_and_swr.sh
@@ -3,6 +3,8 @@
 curl -v http://localhost:4778/max-age/20-seconds/25-age/40-swr
 curl -v http://localhost:4777/max-age/20-seconds/25-age/40-swr
 
+
+curl -v -sS "localhost:4777/max-age/includes-30-max-age-25-age-40-swr-expires-in-120.json" 
 curl -v -sS "localhost:4778/max-age/includes-30-max-age-25-age-40-swr-expires-in-120.json" 
 curl -v -sS "localhost:4778/max-age/includes-30-max-age-25-age-40-swr-expires-in-120.json" 2>&1 | grep "Cache-Control" | tr -d "\n"
 exit $?
