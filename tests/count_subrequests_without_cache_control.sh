4bcdae23c2d25bd107885538c64b204bb473b87b
DracoBlue <jans@dracoblue.de>
2022-01-30 13:11:04 +0100

    Renamed parameter to `X-Ssi-Missing-CC-Count` to avoid having problems with people regexp'ing for "Cache-Control" in the header name

--- tests/count_subrequests_without_cache_control.sh
+++ tests/count_subrequests_without_cache_control.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -v -sS -H 'X-Ssi-Debug: true' "http://localhost:4778/max-age/include-without-cache-control-expires-in-30.json" 2>&1 | grep "X-Ssi-Missing-Cache-Control" | sort -n | tr -d "\n"
+curl -v -sS -H 'X-Ssi-Debug: true' "http://localhost:4778/max-age/include-without-cache-control-expires-in-30.json" 2>&1 | grep "X-Ssi-Missing-CC" | sort -n | tr -d "\n"
 exit $?
--- tests/use_max_age_0_if_max_age_is_broken_on_root.sh
+++ tests/use_max_age_0_if_max_age_is_broken_on_root.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -v -sS curl -v -sS "localhost:4778/max-age/broken-max-age-value" 2>&1 | grep "Cache-Control" | grep -v 'Missing' | tr -d "\n"
+curl -v -sS curl -v -sS "localhost:4778/max-age/broken-max-age-value" 2>&1 | grep "Cache-Control" | tr -d "\n"
 exit $?
